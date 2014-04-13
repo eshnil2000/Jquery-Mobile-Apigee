45e1314efe1d0bfb0a6354fc02fdca0961a066d7
nik <eshnil2000@gmail.com>
2014-04-13 16:36:17 -0700

    added css/images folder

--- js/data-generator.js
+++ js/data-generator.js
@@ -14,34 +14,34 @@
       row += '<td>' + firstNames[0] + '</td>';
       row += '<td>' + lastNames[0] + '</td>';
       
-      row += '<td data-value="1"><img title="active" src="active.png" id="toggle" /></td>';
+      row += '<td data-value="1"><img title="active" src="img/active.png" id="toggle" /></td>';
       
 
       console.log(firstNames[0]);
       row += '</tr>';
       row += '<td>' + firstNames[1] + '</td>';
       row += '<td>' + lastNames[1] + '</td>';
-      row += '<td data-value="1"><img title="active" src="disabled.png" id="toggle" /></td>';
+      row += '<td data-value="1"><img title="active" src="img/disabled.png" id="toggle" /></td>';
       
       row += '</tr>';
       row += '<td>' + firstNames[2] + '</td>';
       row += '<td>' + lastNames[2] + '</td>';
-      row += '<td data-value="1"><img title="active" src="active.png" /></td>';
+      row += '<td data-value="1"><img title="active" src="img/active.png" /></td>';
       
       row += '</tr>';
       row += '<td>' + firstNames[3] + '</td>';
       row += '<td>' + lastNames[3] + '</td>';
-      row += '<td data-value="1"><img title="active" src="active.png" /></td>';
+      row += '<td data-value="1"><img title="active" src="img/active.png" /></td>';
       
       row += '</tr>';
       row += '<td>' + firstNames[4] + '</td>';
       row += '<td>' + lastNames[4] + '</td>';
-      row += '<td data-value="1"><img title="active" src="suspended.png" /></td>';
+      row += '<td data-value="1"><img title="active" src="img/suspended.png" /></td>';
       
       row += '</tr>';
       row += '<td>' + firstNames[5] + '</td>';
       row += '<td>' + lastNames[0] + '</td>';
-      row += '<td data-value="1"><img title="active" src="active.png" /></td>';
+      row += '<td data-value="1"><img title="active" src="img/active.png" /></td>';
 
       row += '</tr>';
       document.writeln(row);
